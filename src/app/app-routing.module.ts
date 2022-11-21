4942daf4fbb1561e0ecc540d1e013b1a5b0e652a
harshitchouhan <harshitsinghchouhan2018@gmail.com>
2022-11-21 13:12:12 +0530

    Student Login Screen created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,48 +1,55 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./auth/login/login.component";
 import { ContentComponent } from "./shared/components/layout/content/content.component";
 import { FullComponent } from "./shared/components/layout/full/full.component";
 import { full } from "./shared/routes/full.routes";
 import { content } from "./shared/routes/routes";
 
-import { AdminGuard } from './shared/guard/admin.guard';
+import { AdminGuard } from "./shared/guard/admin.guard";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard/default',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "dashboard/default",
+    pathMatch: "full",
   },
   {
-    path: 'auth/login',
-    component: LoginComponent
+    path: "auth/login",
+    component: LoginComponent,
   },
   {
-    path: '',
+    path: "student/login",
+    component: LoginComponent,
+  },
+  {
+    path: "",
     component: ContentComponent,
     canActivate: [AdminGuard],
-    children: content
+    children: content,
   },
   {
-    path: '',
+    path: "",
     component: FullComponent,
     canActivate: [AdminGuard],
-    children: full
+    children: full,
   },
   {
-    path: '**',
-    redirectTo: ''
-  }
+    path: "**",
+    redirectTo: "",
+  },
 ];
 
 @NgModule({
-  imports: [[RouterModule.forRoot(routes, {
-    anchorScrolling: 'enabled',
-    scrollPositionRestoration: 'enabled',
-    relativeLinkResolution: 'legacy'
-})],
-],
-  exports: [RouterModule]
+  imports: [
+    [
+      RouterModule.forRoot(routes, {
+        anchorScrolling: "enabled",
+        scrollPositionRestoration: "enabled",
+        relativeLinkResolution: "legacy",
+      }),
+    ],
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
 import { environment } from "src/environments/environment";
 import { AuthService } from "../../shared/services/firebase/auth.service";
 
@@ -9,18 +10,26 @@ import { AuthService } from "../../shared/services/firebase/auth.service";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  public show: boolean = false;
-  public loginForm: FormGroup;
-  public errorMessage: any;
+  show: boolean = false;
+  loginForm: FormGroup;
+  errorMessage: any;
 
-  constructor(private fb: FormBuilder, public authService: AuthService) {
+  pageType: "admin" | "student" = "admin";
+
+  constructor(private fb: FormBuilder, public authService: AuthService, private router: Router) {
     this.loginForm = this.fb.group({
       email: ["test@gmail.com", [Validators.required, Validators.email, Validators.pattern(environment.emailRegex)]],
       password: ["test123", Validators.required],
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.router.url.includes("student")) {
+      this.pageType = "student";
+    } else if (this.router.url.includes("auth")) {
+      this.pageType = "admin";
+    }
+  }
 
   showPassword() {
     this.show = !this.show;
