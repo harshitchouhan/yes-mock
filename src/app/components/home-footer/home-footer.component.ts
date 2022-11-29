4b06b283188261d176ae5a38d4f59c7ce67351da
harshitchouhan <harshitsinghchouhan2018@gmail.com>
2022-11-29 11:51:28 +0530

    footer completed for homepage

--- src/app/components/home-footer/home-footer.component.ts
+++ src/app/components/home-footer/home-footer.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home-footer.component.scss"],
 })
 export class HomeFooterComponent implements OnInit {
+  public today: number = Date.now();
+
   constructor() {}
 
   ngOnInit(): void {}
